5a1a90e5a530324c8c4abc38643769b430a0084b
phawit-a49 <phawit@a49.co.th>
2024-06-26 17:26:07 +0700

    add hero component

---
+++ src/components/navbar/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./hero.scss"
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="textContainer">
+            <h2>Architects 49 .Limited</h2>
+            <h1>Integrated Research and Innovations Department</h1>
+        </div>
+      <div className="imageContainer">
+        <img src="/IRIs Logo_Final_0.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
