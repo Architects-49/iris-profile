89d024e502e6345f0f0b0d38b93b2cd96be2eaba
phawit-a49 <phawit@a49.co.th>
2024-06-26 15:47:29 +0700

    Add page section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Pallalax</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+      {/* <Test/> */}
+    </div>
+  );
 };
 
 export default App;
